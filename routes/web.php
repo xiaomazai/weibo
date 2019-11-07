ceebdd5ae3d255d439d00a2a972b2086df17d0cd
vagrant <vagrant@homestead>
2019-11-07 06:13:34 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ Route::get('/', function () {
 });
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/about', 'StaticPagesController@about')->name('about');
